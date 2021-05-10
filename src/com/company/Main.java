024e84b4a4a0a66382fe5dc0457c2740c7cf3b2d
Ashok-bai <alisher210321@mail.ru>
2021-05-10 15:08:33 +0600

    v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("new Change");
+        System.out.println("njksdfnsdjkfnsj");
     }
 }
